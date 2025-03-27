dbf8b317218bbc9fbfb60674410136d75d387ade
forest-finder <support@knowledgehunter.ai>
2025-03-27 08:00:48 -0400

    Add main.dart with website implementation

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:knowledge_hunter_supportweb/view/screens/website_screen/website_screen.dart';
+import 'package:knowledge_hunter_supportweb/view/screens/termsconditions_screen/termsconditions_screen.dart';
+import 'package:knowledge_hunter_supportweb/view/screens/privacypolicy_screen/privacypolicy_screen.dart';
+import 'package:knowledge_hunter_supportweb/view/screens/supportform_screen/supportform_screen.dart';
+import 'package:knowledge_hunter_supportweb/view/screens/adminlogin_screen/adminlogin_screen.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:knowledge_hunter_supportweb/repository/colors.dart';
+import 'dart:async';
+import 'dart:math' as math;
+import 'package:url_launcher/url_launcher_string.dart';
+import 'package:flutter/painting.dart';
+
+[Rest of your main.dart file content...]
