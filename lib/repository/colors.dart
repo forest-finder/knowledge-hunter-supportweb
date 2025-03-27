96c43c81e011059e2d00397a75cfe061a3633934
forest-finder <support@knowledgehunter.ai>
2025-03-27 08:00:54 -0400

    Add colors.dart with app color definitions

---
+++ lib/repository/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color futureTurquoise = Color(0xFF35D7CA);
+  static const Color futureGreen = Color(0xFF2A9D8F);
+  static const Color navyBlueGreen = Color(0xFF264653);
+  static const Color darkestNavy = Color(0xFF1B2B35);
+  static const Color tangerineOrange = Color(0xFFE76F51);
+}
